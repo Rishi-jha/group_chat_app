d7deaf6ad553547a28fd06eea335e2127a5c4322
rishikesh <rishikesh@melvault.com>
2023-02-06 09:04:43 +0530

    group chat app

--- chat_group/chat_group/settings.py
+++ chat_group/chat_group/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zsau12c4$m-jmgu)((_gtmi@puoe9raet0jc@h*piarx4_we9x"
+# SECRET_KEY = "django-insecure-zsau12c4$m-jmgu)((_gtmi@puoe9raet0jc@h*piarx4_we9x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
